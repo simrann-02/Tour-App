b1fbe7135c6202ab81c1ae997e5cd842acf1990a
simrann 02 <simran292002@gmail.com>
2023-09-24 01:14:40 +0530

    tour-app project created

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+function Card({id,image,info,price,name,removeTour}){
+    const [readMore, setReadMore]=useState(false);
+    const description = readMore? info:`${info.substring(0,200)}....`;
+
+    function readMoreHandler(){
+        setReadMore(!readMore);
+    }
+   
+    return (
+        <div className="card">
+            <img src={image} className="image"></img>
+            <div className='tour-info'>
+                <div className="tour-details">
+                    <h4 className='tour-price'>₹{price}</h4>
+                    <h4 className="tour-name">{name}</h4>
+                </div>
+                <div className="description">{description}
+                <span onClick={readMoreHandler} className='read-more'>{readMore ? `Show Less`: `Read More`}</span></div>
+            </div>
+            <button className="btn-read" onClick={()=>removeTour(id)}>
+                Not Interested
+            </button>
+        </div>
+    )
+}
+export default Card;
